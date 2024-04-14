05dfc0c1c247859f1829864784d7b93eb9a7de25
LiDing <72634327+linkedlist771@users.noreply.github.com>
2024-04-14 12:44:37 +0800

    Feature: build the basic request and response structure

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
-use actix_web::{get, post, web,  body::BoxBody,http::header::ContentType, App, middleware::Logger, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::env;
+use actix_web::{
+    body::BoxBody, get, http::header::ContentType, middleware::Logger, post, web, App, HttpRequest,
+    HttpResponse, HttpServer, Responder,
+};
 use clap::{App as ClapApp, Arg};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BaseRequest {
-    model: Option<String>,
-}
+use std::env;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct EmbeddingRequest {
+    model: Option<String>,
     input: Vec<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct BaseResponse {
-    object: String,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -35,75 +28,52 @@ struct Embedding {
 
 #[derive(Deserialize, Serialize, Debug)]
 struct EmbeddingResponse {
+    object: String,
     data: Vec<Embedding>,
     model: String,
     usage: Usage,
 }
 
-#[derive(Serialize)]
-struct MyObj {
-    name: &'static str,
-}
-
-// Responder
-impl Responder for MyObj {
-    type Body = BoxBody;
-
-    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
-        let body = serde_json::to_string(&self).unwrap();
-
-        // Create response and set content type
-        HttpResponse::Ok()
-            .content_type(ContentType::json())
-            .body(body)
-    }
-}
-
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    log::info!("Handling GET / request");
-    HttpResponse::Ok().body(req_body)
-}
-
-
-
 #[get("/get_model_types")]
 async fn get_model_types() -> impl Responder {
-    let dummy_model_types = json!({
-        "model_types": [
-            {
-                "model": "cl100k_base",
-                "description": "CL-100k base model"
-            },
-            {
-                "model": "cl100k_large",
-                "description": "CL-100k large model"
-            },
-            {
-                "model": "cl100k_xlarge",
-                "description": "CL-100k xlarge model"
-            }
+    let dummy_model_types = json!([
+        {
+            "label": "text2vec-base-chinese",
+            "value": 1
+        }
         ]
-    });
+    );
 
     HttpResponse::Ok().json(dummy_model_types)
-
 }
 
-
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/embeddings")]
+async fn embeddings(embedding_request: web::Json<EmbeddingRequest>) -> impl Responder {
+    let model_name = embedding_request.model.clone().unwrap();
+    let input = embedding_request.input.clone();
+    let mut dummy_embeddings = Vec::<Embedding>::new();
+    for (i, text) in input.iter().enumerate() {
+        let embedding = Embedding {
+            object: "embedding".to_string(),
+            index: i as i32,
+            embedding: vec![0.1, 0.2, 0.3],
+        };
+        dummy_embeddings.push(embedding);
+    }
+    let usage = Usage {
+        prompt_tokens: 0,
+        total_tokens: 0,
+    };
+    let embedding_response = EmbeddingResponse {
+        object: "list".to_string(),
+        data: dummy_embeddings,
+        model: model_name,
+        usage: usage,
+    };
+    HttpResponse::Ok().json(embedding_response)
 }
 
 // auto reloading :  cargo watch -x run --host 0.0.0.0 --port 8848
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -111,27 +81,31 @@ async fn main() -> std::io::Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
     env_logger::init(); // 初始化日志记录器
     let matches = ClapApp::new("MyApp")
-    .arg(Arg::with_name("host")
-        .long("host")
-        .default_value("127.0.0.1")
-        .help("host address to bind"))
-    .arg(Arg::with_name("port")
-        .long("port")
-        .default_value("8848")
-        .help("port to listen on"))
-    // .arg(Arg::with_name("models_dir_path")
-    //     .long("models_dir_path")
-    //     .required(true)
-    //     .help("path to the models directory"))
-    // .arg(Arg::with_name("use_gpu")
-    //     .long("use_gpu")
-    //     .takes_value(false)
-    //     .help("flag to use GPU"))
-    // .arg(Arg::with_name("tokenizer_model")   
-    //     .long("tokenizer_model")
-    //     .default_value("cl100k_base")
-    //     .help("tokenizer model name"))
-    .get_matches();
+        .arg(
+            Arg::with_name("host")
+                .long("host")
+                .default_value("127.0.0.1")
+                .help("host address to bind"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .long("port")
+                .default_value("8848")
+                .help("port to listen on"),
+        )
+        // .arg(Arg::with_name("models_dir_path")
+        //     .long("models_dir_path")
+        //     .required(true)
+        //     .help("path to the models directory"))
+        // .arg(Arg::with_name("use_gpu")
+        //     .long("use_gpu")
+        //     .takes_value(false)
+        //     .help("flag to use GPU"))
+        // .arg(Arg::with_name("tokenizer_model")
+        //     .long("tokenizer_model")
+        //     .default_value("cl100k_base")
+        //     .help("tokenizer model name"))
+        .get_matches();
 
     let host = matches.value_of("host").unwrap();
     let port = matches.value_of("port").unwrap().parse::<u16>().unwrap();
@@ -146,12 +120,12 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         let logger = Logger::default();
-        App::new()
-            .wrap(logger)
-            .service(hello)
-            .service(echo)
-            .service(get_model_types)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().wrap(logger).service(
+            web::scope("/v1")
+                .service(get_model_types)
+                .service(embeddings),
+        )
+        // .route("/hey", web::get().to(manual_hello))
     })
     .bind((host, port))?
     .run()
